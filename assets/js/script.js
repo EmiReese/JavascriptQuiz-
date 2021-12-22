6ce5699996fdb459b39024e976ea67d61771b522
Emily Reese <emilyreese.ga@gmail.com>
2021-12-21 23:47:48 -0500

    I have added CSS styling to the quiz. The buttons work, the page continues to the next sections correctly and the score is presented at the end of the quiz.

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,43 +16,26 @@ var quizQuestions = [
         answers: ["evaluates a condition in parentheses and, if the result is true, executes a block of code.", "returns undefined", "puts together multiple strings of information", "none of the above"],
         correct: "evaluates a condition in parentheses and, if the result is true, executes a block of code."
     },
-    // {
-    //     question: "What does blue and yellow make?",
-    //     anwsers: [
-    //         {a: "purple"},
-    //         {b: "green"},
-    //         {c: "orange"}
-    //     ],
-    //     correct: answers["b"]
-    // }
-
     ];
+
     var startContainer = document.getElementById('startContainer');
     var startButton = document.getElementById('start');
     var questionContainer = document.getElementById('questionContainer');
     var scoreContainer = document.getElementById('scoreContainer');
     var currentQuestionIndex = 0;
-    var olEl = document.createElement('ol');
     var answersHTML = document.getElementById('answers')
     var score = 0;
-  
-
-    function startgame () {
-        console.log('click');
-        showQuestions();
-    }
-    // To start the game, we need to call upon the showQuestions functions below. After the user clicks the button, the question will appear. 
 
 
 const showQuestions = () => {
-    answersHTML.innerHTML = ""
+    answersHTML.innerHTML = "";
+        // In the first line, we are setting the variable current question to the quizQuestions Object (with all the questions and answers) and we are adding the currentQuestionIndex which will help us target each individual question.
     var currentQuestion = quizQuestions[currentQuestionIndex];
+        // In the second line we are setting the question title to the questionTitle variable we established in index.HTML
     var questionTitle = document.getElementById('questionTitle');
+      //In the third line, we are changing the questionTitle to be the appropriate question title for the question the user is answering. We are doing this by targeting the "currentQuestion" variable we established above and then targeting the property "question" to target the question title in js.
     questionTitle.innerHTML = currentQuestion.question;
 
-    // In the first line, we are setting the variable current question to the quizQuestions Object (with all the questions and answers) and we are adding the currentQuestionIndex which will help us target each individual question.
-    // In the second line we are setting the question title to the questionTitle variable we established in index.HTML
-    //In the third line, we are changing the questionTitle to be the appropriate question title for the question the user is answering. We are doing this by targeting the "currentQuestion" variable we established above and then targeting the property "question" to target the question title in js.
 
     // In this section of code, we are adding the answers to the buttons for each indiviudal question. 
         currentQuestion.answers.forEach( (answer) => {
@@ -61,9 +44,8 @@ const showQuestions = () => {
             button.className = "choice-btn"
             answersHTML.append(button);
         });
-       // Below, is our conditional statement. At this moment, this piece of code is not working. I am trying to compare the answer the user clicks on to the "correct" answer displayed in the object "quizQuestions" above.
-    console.log(`current question: `,currentQuestion)
-
+     
+        //We are adding the click event to each answer-button. Then, we are comparing whether the option the user chose is the same as the "correct" answer. This is console.logged for the programmer. 
     var choices = document.getElementsByClassName("choice-btn")
     Array.from(choices).forEach((choice)=> {
         choice.addEventListener('click',  (e) => {
@@ -73,22 +55,23 @@ const showQuestions = () => {
                score += 1;
            } else {
                 console.log('loser')
+                // this is for you. I did not forget this here. 
                score += 0;
                 timeleft -= (1000 * 5);
            }
-           
+        
+
     if(quizQuestions.length > currentQuestionIndex+1) {
         currentQuestionIndex = currentQuestionIndex +1
         showQuestions();
     } else {
         console.log("end of quiz")
-        // Hey Emily...fix your display of the score here!
-        scoreContainer.innerHTML = "Score";
+      
+        scoreContainer.innerHTML = "Score :  " + score;
         startContainer.classList.add('hidden');
         questionContainer.classList.add('hidden');
         scoreContainer.classList.remove('hidden');
     }
-           
         });
     })
 }
@@ -103,7 +86,7 @@ const showQuestions = () => {
     });
     //----------------------------------------------------------------------
 //   This is our timer countdown from 60 seconds
-var timeleft = 60000;
+var timeleft = 30000;
 var quizTimer = setInterval(()=>{
   if(timeleft <= 0){
     clearInterval(quizTimer);
@@ -113,20 +96,3 @@ var quizTimer = setInterval(()=>{
   }
   timeleft -= 1000;
 }, 1000);
-
-// This is a function I had created to excute the conditional statement so the quiz can go to the next question after being answered
-// eachAnswer.addEventListener('click',  function () 
-// {
-//     for (var i = 0; i < answers.length; i++){
-//         if (eachAnswer[i] === quizQuestions.correct) {
-//             score =+ 1;
-//             quizQuestions++
-//         }
-//         else {
-//             score += 0;
-//             quizQuestions++;
-//             timer -= (1000 * 5);
-//         }
-//     }
-// });
-    
